7f867e27227db98aa6ceadeb338f89bf7ff53ab9
Harshal Sheth <hsheth2@gmail.com>
2018-07-07 22:55:19 -0400

    Update API documentation

--- api.php
+++ api.php
@@ -74,7 +74,7 @@
 			<p class="lead">
 				Example: Sending Parameters
 				<br />
-				<pre>$ curl "http://api.when2water.org/?zip=90210&json=true"</pre>
+				<pre>$ curl "https://api.when2water.org/?zip=90210&json=true"</pre>
 			</p>
 		</div>
 		
@@ -129,17 +129,17 @@
 			</p>
 			<p class="lead">
 				Example: Getting an Unformatted Response
-<pre>$ curl "api.when2water.org/?zip=99801"
+<pre>$ curl "https://api.when2water.org/?zip=99801"
 -1</pre>
 			</p>
 			<p class="lead">
 				Example: Getting a JSON Formatted Response
-<pre>$ curl "api.when2water.org/?zip=99801&json=true"
+<pre>$ curl "https://api.when2water.org/?zip=99801&json=true"
 {"signal":-1}</pre>
 			</p>
 			<p class="lead">
 				Example: Getting an Error Response Code, and an HTTP 200 OK, from an Invalid Request
-<pre>$ curl -i "api.when2water.org/?zip=99999"
+<pre>$ curl -i "https://api.when2water.org/?zip=99999"
 HTTP/1.1 200 OK
 Date: Sat, 28 Jun 2014 03:41:07 GMT
 Server: Apache mod_fcgid/2.3.10-dev
